c33a9d4713a0f6f2fdfd9a35f09e092cbf802402
Robin Erlacher <rerlache@gmail.com>
2022-01-18 20:09:25 +0100

    made both web apps working, implemented the blazor solution to this project

--- robinerlacher.online/BlazorServerApp/Startup.cs
+++ robinerlacher.online/BlazorServerApp/Startup.cs
@@ -1,4 +1,6 @@
+using BlazorServerApp.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -9,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace robinerlacher.online
+namespace BlazorServerApp
 {
     public class Startup
     {
@@ -21,9 +23,12 @@ namespace robinerlacher.online
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,11 +50,10 @@ namespace robinerlacher.online
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
