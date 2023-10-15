6db948155352f4a62734851e2c680f9ea7ed3c45
Robin Erlacher <rerlache@gmail.com>
2023-10-15 21:33:19 +0200

    added the login controller

---
+++ robinerlacher.online/API/Controllers/General/LoginController.Gets.cs
@@ -0,0 +1,32 @@
+﻿using API.Data.General;
+using API.Services.GeneralService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.General
+{
+    public partial class LoginController
+    {
+        [HttpGet]
+        public async Task<ActionResult<Tuple<string, UserDTO>>> WithData([FromHeader] string userName, [FromHeader] string password)
+        {
+            string ip = Request.HttpContext?.Request.Headers["X-Forwarded-For"].ToString();
+            if (ip == null)
+            {
+                ip = HttpContext.GetServerVariable("REMOTE_ADDR");
+            }
+            Tuple<string, User> result = await _loginService.WithDataAsync(userName, password, ip);
+            if (result.Item2 == null)
+            {
+                return BadRequest(result.Item1);
+            }
+            return Ok(new Tuple<string, UserDTO>(result.Item1, _mapper.Map<UserDTO>(result.Item2)));
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<UserLoginHistoryDTO>>> GetHistory([FromHeader] int userId)
+        {
+            List<UserLoginHistory> history = await _loginService.GetLoginHistoryAsync(userId);
+            UserLoginHistoryDTO[] dtoLogins = _mapper.Map <UserLoginHistoryDTO[]>(history).ToArray();
+            return Ok(dtoLogins);
+        }
+    }
+}
---
+++ robinerlacher.online/API/Controllers/General/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using API.Services.GeneralService;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.General
+{
+    [ApiController]
+    [Route("general/[controller]/[action]")]
+    public partial class LoginController : ControllerBase
+    {
+        private readonly ILoginService _loginService;
+        private readonly IMapper _mapper;
+
+        public LoginController(ILoginService applicationService, IMapper mapper)
+        {
+            _loginService = applicationService;
+            _mapper = mapper;
+        }
+    }
+}
